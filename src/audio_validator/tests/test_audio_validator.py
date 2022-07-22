98639643d6c82a1b51cfe3554a5ab2cec6bf07a8
rezashakeri <rezashakeri2648@gmail.com>
2022-07-22 05:02:28 +0430

    add django to requirements.txt

--- src/audio_validator/tests/test_audio_validator.py
+++ src/audio_validator/tests/test_audio_validator.py
@@ -1,6 +1,15 @@
+from multiprocessing import Value
 import pytest
 import os
+from audio_validator.validator import AudioValidator
+import pygame
+
 
 class TestMp3AudioType:
     def test_if_validator_accepted_wrong_type(self):
-        pass
+        module_dir = os.path.dirname(__file__)
+        file_path = os.path.join(module_dir, "test_files", "mp3", "test_image.mp3")
+        pygame.mixer.init()
+        file = pygame.mixer.music.load(file_path)
+        audio_validator = AudioValidator(audio_type="mp3")
+        audio_validator(value=file)
